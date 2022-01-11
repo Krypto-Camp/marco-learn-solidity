9555fb1324e719faa8a9c74b77ee3e2bfcbd1e9d
marco <a0931900802@gmail.com>
2022-01-11 13:04:49 +0800

    20220111

--- contracts/practice/lesson2_transfer.sol
+++ contracts/practice/lesson2_transfer.sol
@@ -1,28 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
-contract homework {
-
-    // mapping address
-    mapping (address => uint) public balancesReceived;
+contract practice2 {
+    // mapping address(key: value)
+    // Balances[address] = balance 
+    mapping (address => uint) public Balances;
 
     // put money into contract
     function receiveMoney() external payable {
-        balancesReceived[msg.sender] += msg.value;
+        Balances[msg.sender] += msg.value; // sender => money
     }
 
     // get money from contract
     function withdrawMoney(address payable _receiver, uint _amount) external {
         _receiver.transfer(_amount);
-        balancesReceived[_receiver] += _amount;
+        Balances[_receiver] += _amount;
     }
 
     // get total money from contract
     function getContractBalance() external view returns (uint) {
         return address(this).balance;
     }
-
 }
-
-
